d96629afbaaf6272b642dcebd4e2b77c42266973
Peter Centellini <peter.centellini@redesajn.com>
2021-10-05 16:17:44 +0200

    #Tutorial - Wpf Modern Flat UI: Initial setup of project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tutorials
 Different tutorials, mostly picked up from Youtube
+
+1. Wpf C# Professional Modern Flat UI Tutorial - Added 211001, from Youtube
---
+++ Wpf Professional Modern Flat UI Tutorial/ModernDesign/Core/ObservableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernDesign.Core {
+  public class ObservableObject : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void RaisePropertyChanged([CallerMemberName] string name = null) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+  }
+}
