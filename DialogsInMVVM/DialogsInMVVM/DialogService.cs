27070cd8a051ab9b816308011ff23e94e1efef0e
Peter Centellini <peter.centellini@cytiva.com>
2022-03-09 18:36:40 +0100

    #DialogsInMVVM: Initial commit

---
+++ DialogsInMVVM/DialogsInMVVM/DialogService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogsInMVVM
+{
+    public interface IDialogService
+    {
+        void ShowDialog();
+    }
+    public class DialogService : IDialogService
+    {
+        public void ShowDialog()
+        {
+            var dialog = new DialogWindow();
+
+            dialog.ShowDialog();
+        }
+    }
+}
---
+++ DialogsInMVVM/DialogsInMVVM/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogsInMVVM
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private IDialogService _dialogService = new DialogService();
+
+        private DelegateCommand _showDialog;
+        public DelegateCommand ShowDialog => _showDialog ?? (_showDialog = new DelegateCommand(ExecuteShowDialog));
+
+        private void ExecuteShowDialog()
+        {
+            _dialogService.ShowDialog();
+        }
+    }
+}
