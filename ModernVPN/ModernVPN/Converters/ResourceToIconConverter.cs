9eeca7a9219b7ae5ff4f859926949aaa31a3a436
Peter Centellini <peter.centellini@cytiva.com>
2022-06-27 16:53:00 +0200

    #ModernVPN: initial Committ

---
+++ ModernVPN/ModernVPN/Converters/ResourceToIconConverter.cs
@@ -0,0 +1,38 @@
+﻿using ModernVPN.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ModernVPN.Converters
+{
+    internal class ResourceToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            switch ((WindowIcons)value)
+            {
+                case WindowIcons.WindowClose:
+                    return Application.Current.TryFindResource("2ColIconSm");
+                case WindowIcons.WindowMinimize:
+                    return Application.Current.TryFindResource("SensorgramIconSm");
+                case WindowIcons.WindowMaximize:
+                    return Application.Current.TryFindResource("PlotSm");
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ModernVPN/ModernVPN/Enums/WindowIcons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernVPN.Enums
+{
+    public enum WindowIcons
+    {
+        WindowMinimize,
+        WindowMaximize,
+        WindowClose        
+    }
+}
