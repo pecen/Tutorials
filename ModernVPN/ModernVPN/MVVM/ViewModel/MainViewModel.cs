820ce4b37d4a77edb0d34c78f6b064eec05e43b4
Peter Centellini <peter.centellini@cytiva.com>
2022-06-27 17:54:57 +0200

    #ModernVPN: Completed MainWindow basic functionality

---
+++ ModernVPN/ModernVPN/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ModernVPN.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ModernVPN.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        // Commands
+        public RelayCommand MoveWindowCommand { get; set; }
+        public RelayCommand ShutdownWindowCommand { get; set; }
+        public RelayCommand MaximizeWindowCommand { get; set; }
+        public RelayCommand MinimizeWindowCommand { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Application.Current.MainWindow.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            MoveWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.DragMove(); });
+            ShutdownWindowCommand = new RelayCommand(o => { Application.Current.Shutdown(); });
+            MaximizeWindowCommand = new RelayCommand(o =>
+            {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                }
+            });
+            MinimizeWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.WindowState = WindowState.Minimized; });
+        }
+    }
+}
