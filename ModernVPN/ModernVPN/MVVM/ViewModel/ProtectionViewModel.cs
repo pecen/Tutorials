b1876722db4d6fdd3f7da666086998c477ba5a6f
Peter Centellini <peter.centellini@cytiva.com>
2022-06-27 21:46:28 +0200

    #ModernVPN: Added ServerModel. Added major part to ProectionView with Styling. Added themes. Added icon for SettingsView.

--- ModernVPN/ModernVPN/MVVM/ViewModel/ProtectionViewModel.cs
+++ ModernVPN/ModernVPN/MVVM/ViewModel/ProtectionViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ModernVPN.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,18 @@ namespace ModernVPN.MVVM.ViewModel
 {
     public class ProtectionViewModel
     {
+        public ObservableCollection<ServerModel> Servers { get; set; }
+        
         public ProtectionViewModel()
         {
-
+            Servers = new ObservableCollection<ServerModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                Servers.Add(new ServerModel
+                {
+                    Country = "USA"
+                });
+            }
         }
     }
 }
